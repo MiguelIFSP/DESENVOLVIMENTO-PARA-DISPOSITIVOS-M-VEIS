be34bbf9d32f8e7e9a14f6f4741eeccfdc1d65da
MiguelIFSP <miguel.baptista@aluno.ifsp.edu.br>
2026-09-02 21:43:14 -0300

    Aula 02/09/2026

---
+++ 02-09-2026/lib/pergunta.dart
@@ -0,0 +1,8 @@
+class Pergunta {
+  const Pergunta(this.texto, this.respostas);
+
+  final String texto;
+  final List<String> respostas;
+}
+
+//
---
+++ 02-09-2026/lib/questoes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_application_2/pergunta.dart';
+
+const questoes = [
+  Pergunta('1 + 2 = ?', ['3', '2', '4', '5']),
+  Pergunta('2 + 3 = ?', ['2', '5', '4', '3']),
+  Pergunta(
+    'Qual das opções abaixo está escrita corretamente?',
+    ['Exceção', 'Excensão', 'Excessão', 'Excessao'],
+  ),
+  Pergunta('Qual animal não é preto e branco?',
+      ['Zebra', 'Panda', 'Gambá', 'Tigre']),
+  Pergunta('Qual a capital do Brasil?',
+      ['Brasília', 'Rio de Janeiro', 'São Paulo', 'Salvador']),
+];
